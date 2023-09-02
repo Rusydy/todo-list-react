ea83890781d35ea148d8a4058bfce2384477dcde
Rusydy <rusydymuhiddin@gmail.com>
2023-09-02 15:04:08 +0700

    Add basic HTML structure with JSX
    - React basics:
      - React fragment
      - className
      - htmlFor

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,26 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import "./styles.css"
 
-function App() {
-    const [count, setCount] = useState(0);
-
+export default function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={reactLogo} className="App-logo" alt="logo" />
-                <img src={viteLogo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>App.jsx</code> and save to test HMR updates.
-                </p>
-                <p>
-                    <button
-                        className="App-link"
-                        onClick={() => setCount((count) => count + 1)}
-                    >
-                        count is: {count}
-                    </button>
-                </p>
-            </header>
-        </div>
-    );
+    <> {/* This is a React fragment. It lets us return multiple elements.*/}
+        <form className="new-task">
+            <div className="task-name"> {/* className is the React equivalent of class in HTML */}
+                <label htmlFor="task-name">Task Name</label> {/* htmlFor is the React equivalent of for in HTML */}
+                <input type="text" id="task-name" />
+            </div>
+            <button className="btn" >Add Task</button>
+        </form>
+        <h1 className="title"> Todo List </h1>
+        <ul className="task-list">
+            <li className="task">
+                <label >Task 1</label>
+                <button className="btn btn-danger">Delete</button>
+            </li>
+            <li className="task">
+                <label >Task 2</label>
+                <button className="btn btn-danger">Delete</button>
+            </li>
+        </ul>
+    </>
+    )
 }
-
-export default App;
