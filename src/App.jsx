4ae0e6bf4865e2bdd72c695230beab81383cc261
Rusydy <rusydymuhiddin@gmail.com>
2023-09-02 19:48:37 +0700

    minor: update code lint to match with standard style

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import "./styles.css"
+import "./styles.css";
 import { useState } from "react";
 
 export default function App() {
@@ -25,41 +25,52 @@ export default function App() {
             return prevTasks.map((task) => {
                 if (task.id === id) {
                     return { ...task, isCompleted };
-                } 
-                    return task;
+                }
+                return task;
             });
         });
     }
 
     return (
-    <>
-        <form onSubmit={handleSubmit} className="new-task">
-            <div className="task-name">
-                <label htmlFor="task-name">Task Name</label>
-                <input 
-                    value={newTask}
-                    onChange={(e) => setNewTask(e.target.value)}
-                    type="text" 
-                    id="task-name" 
-                />
-            </div>
-            <button className="btn" >Add Task</button>
-        </form>
-        <h1 className="title"> Todo List </h1>
-        <ul className="task-list">
-            {tasks.map((task) => {
-                return (
-                    <li className="task" key={task.id}>
-                        <label>
-                            <input type="checkbox" checked={task.isCompleted} onChange={(e) => toggleTask(task.id, e.target.checked)} />
-                            {task.name}
-                        </label>
+        <>
+            <form onSubmit={handleSubmit} className="new-task">
+                <div className="task-name">
+                    <label htmlFor="task-name">Task Name</label>
+                    <input
+                        value={newTask}
+                        onChange={(e) => setNewTask(e.target.value)}
+                        type="text"
+                        id="task-name"
+                    />
+                </div>
+                <button className="btn">Add Task</button>
+            </form>
+            <h1 className="title"> Todo List </h1>
+            <ul className="task-list">
+                {tasks.map((task) => {
+                    return (
+                        <li className="task" key={task.id}>
+                            <label>
+                                <input
+                                    type="checkbox"
+                                    checked={task.isCompleted}
+                                    onChange={(e) =>
+                                        toggleTask(task.id, e.target.checked)
+                                    }
+                                />
+                                {task.name}
+                            </label>
 
-                        <button className="btn btn-danger">Delete</button>
-                    </li>
-                )
-            })}
-        </ul>
-    </>
-    )
+                            <button
+                                onClick={() => deleteTask(task.id)}
+                                className="btn btn-danger"
+                            >
+                                Delete
+                            </button>
+                        </li>
+                    );
+                })}
+            </ul>
+        </>
+    );
 }
